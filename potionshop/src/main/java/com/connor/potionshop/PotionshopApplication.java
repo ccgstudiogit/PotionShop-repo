5267643fc113ac87beaf94605159db5f27fb29d6
ccgstudiogit <trempe.connor@gmail.com>
2026-03-12 15:27:25 -0500

    application.properties adjustments
    Basically copy/pasted the application.properties stuff from my JavaSpringBootCourse-repo and made some slight changes to the connection settings.

--- potionshop/src/main/java/com/connor/potionshop/PotionshopApplication.java
+++ potionshop/src/main/java/com/connor/potionshop/PotionshopApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PotionshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PotionshopApplication.class, args);
+        SpringApplication.run(PotionshopApplication.class, args);
 	}
 }
