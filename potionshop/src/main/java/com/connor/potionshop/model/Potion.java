c408978c20f64b201d88842d64a2b2ca9c96f475
ccgstudiogit <trempe.connor@gmail.com>
2026-03-12 17:47:13 -0500

    Update Potion.java
    Finished creating the constructors, getters, and setters for Potion. Also added a column constraint to name, type, and price to make sure the database also enforces those rules.

--- potionshop/src/main/java/com/connor/potionshop/model/Potion.java
+++ potionshop/src/main/java/com/connor/potionshop/model/Potion.java
@@ -11,13 +11,70 @@ public class Potion {
     private Integer id;
 
     @NotBlank
+    @Column(nullable = false) // Enforces constraints in the database as well
     private String name;
 
     @Enumerated(EnumType.STRING)
+    @NotNull
+    @Column(nullable = false)
     private PotionType type;
 
     private String effect;
 
     @NotNull
+    @Column(nullable = false)
     private Integer price;
+
+    // Empty constructor is required for JPA
+    protected Potion() { }
+
+    public Potion(String name, PotionType type, String effect) {
+        this.name = name;
+        this.type = type;
+        this.effect = effect;
+        price = 0;
+    }
+
+    public Potion(String name, PotionType type, String effect, Integer price) {
+        this.name = name;
+        this.type = type;
+        this.effect = effect;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PotionType getType() {
+        return type;
+    }
+
+    public void setType(PotionType type) {
+        this.type = type;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public void setEffect(String effect) {
+        this.effect = effect;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
