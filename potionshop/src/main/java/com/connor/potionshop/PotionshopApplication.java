8da325f5db47712d43aa3626afda1291752230e6
ccgstudiogit <trempe.connor@gmail.com>
2026-03-12 15:02:26 -0500

    Imported Spring Boot base project
    Created a base Spring Boot project via Spring Initializr with the following dependencies: Spring Web, Spring Data JPA, and PostgreSQL Driver.

---
+++ potionshop/src/main/java/com/connor/potionshop/PotionshopApplication.java
@@ -0,0 +1,12 @@
+package com.connor.potionshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotionshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotionshopApplication.class, args);
+	}
+}
---
+++ potionshop/src/test/java/com/connor/potionshop/PotionshopApplicationTests.java
@@ -0,0 +1,12 @@
+package com.connor.potionshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotionshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
