f438f8a1ac0e7f43610338db991d24270d872c0e
ccgstudiogit <trempe.connor@gmail.com>
2026-04-09 19:08:04 -0500

    State refactor for ingredients list in potion-form-utils.js
    Instead of passing a bunch of parameters around, I think the state object so much cleaner. I also renamed some of the function and variable names to better describe what they are doing and slightly shorten the lengths of the names as well.

--- frontend/scripts/ui/potions/potion-form-utils.js
+++ frontend/scripts/ui/potions/potion-form-utils.js
@@ -106,8 +106,8 @@ export function createEffectInput(parent) {
   };
 }
 
-export function createIngredientsInputList(parent, activeIngredients, remainingIngredientOptions) {
-  if (!Array.isArray(activeIngredients) || !Array.isArray(remainingIngredientOptions)) {
+export function createIngredientsInputList(parent, selectedIngredients, availableIngredients) {
+  if (!Array.isArray(selectedIngredients) || !Array.isArray(availableIngredients)) {
     console.error("startingIngredients and selectableIngredients need to be an array!");
     return;
   }
@@ -126,64 +126,76 @@ export function createIngredientsInputList(parent, activeIngredients, remainingI
   const ingredientsContainer = elementFactory.createAndAppendElement('div', null, ingredientsListContainer);
 
   // Create the ingredient dropdown container. The actual dropdown element is created in refreshAddIngredientDropdown()
-  const addIngredientDropdownContainer = elementFactory.createAndAppendElement('div', 'add-potion-form-ingredient-dropdown-container', ingredientsListContainer);
+  const ingredientDropdownContainer = elementFactory.createAndAppendElement('div', 'add-potion-form-ingredient-dropdown-container', ingredientsListContainer);
+
+  const state = {
+    selectedIngredients,
+    availableIngredients,
+    ingredientsContainer,
+    dropdownContainer: ingredientDropdownContainer
+  }
   
   // Render the list. The list is continually updated via recursion as the user adds/removes ingredients
-  updateIngredientList(ingredientsContainer, activeIngredients, remainingIngredientOptions, addIngredientDropdownContainer);
+  renderIngredientList(state);
 }
 
-function updateIngredientList(ingredientsContainer, activeIngredients, remainingIngredientOptions, dropdownContainer) {
-  ingredientsContainer.innerHTML = '';
+function renderIngredientList(state) {
+  const selectedIngredients = [...state.selectedIngredients];
+  const ingredientsContainer = state.ingredientsContainer;
+  ingredientsContainer.innerHTML = ''; // Clear the displayed ingredients
 
-  activeIngredients.forEach(ingredientObject => {
-    const ingredientDOMElement = createIngredient(ingredientObject);
+  selectedIngredients.forEach(ingredientObject => {
+    const ingredientDOMElement = renderIngredientRow(ingredientObject);
 
     // Only add a remove button if there are 2 or more ingredients. Prevents the user from having no ingredients selected
-    if (activeIngredients.length > 1) {
+    if (selectedIngredients.length > 1) {
       buttonFactory.createAndAppendButton('Remove', 'add-potion-form-remove-ingredient-button', ingredientDOMElement.infoContainer, () => {
-        // Get updated active/remaining options lists with the removed active potion and then update the DOM
-        const updated = removeFromActiveIngredients(ingredientObject, activeIngredients, remainingIngredientOptions);
-        updateIngredientList(ingredientsContainer, updated.active, updated.options, dropdownContainer);
+        // Get updated state after removing the ingredient and then update the DOM
+        removeIngredient(ingredientObject, state);
+        renderIngredientList(state);
       });
     }
 
     ingredientsContainer.appendChild(ingredientDOMElement.root);
   });
 
-  refreshAddIngredientDropdown(ingredientsContainer, activeIngredients, remainingIngredientOptions, dropdownContainer);
+  renderIngredientDropdown(state);
 }
 
 // These functions could handle adding/removing an ingredient to the list and returning both an updated active ingredient list and a dropdown ingredient list
-function addToActiveIngredients(ingredientToAddId, activeIngredients, remainingIngredientOptions) {
+function addIngredient(ingredientToAddId, state) {
+  const updatedAvailableIngredients = [...state.availableIngredients];
+
   // Remove the desired ingredient from the remaining ingredient options that are currently in the dropdown
-  const index = remainingIngredientOptions.findIndex(ingredient => ingredient.id === ingredientToAddId);
-  const [ingredientObjToAdd] = remainingIngredientOptions.splice(index, 1);
+  const index = updatedAvailableIngredients.findIndex(ingredient => ingredient.id === ingredientToAddId);
+  const [ingredientObjToAdd] = updatedAvailableIngredients.splice(index, 1);
 
   // Add the desired ingredient to the active ingredients, so when the ingredients list is rebuilt it's included
-  activeIngredients.push(ingredientObjToAdd);
+  const updatedSelectedIngredients = [...state.selectedIngredients];
+  updatedSelectedIngredients.push(ingredientObjToAdd);
 
-  return {
-    active: activeIngredients,
-    options: remainingIngredientOptions
-  }
+  state.selectedIngredients = updatedSelectedIngredients;
+  state.availableIngredients = updatedAvailableIngredients;
 }
 
-function removeFromActiveIngredients(ingredientToRemove, activeIngredients, remainingIngredientOptions) {
+function removeIngredient(ingredientToRemove, state) {
   const ingredientToRemoveId = ingredientToRemove.id;
 
   // Remove this ingredient from the active ingredients
-  activeIngredients = activeIngredients.filter(ingredient => ingredient.id !== ingredientToRemoveId);
+  const updatedSelectedIngredients = [...state.selectedIngredients].filter(ingredient => ingredient.id !== ingredientToRemoveId);
 
   // Put this ingredient back in the ingredient selection dropdown
-  remainingIngredientOptions.push(ingredientToRemove);
+  const updatedAvailableIngredients = [...state.availableIngredients];
+  updatedAvailableIngredients.push(ingredientToRemove);
 
-  return {
-    active: activeIngredients,
-    options: remainingIngredientOptions
-  }
+  state.selectedIngredients = updatedSelectedIngredients;
+  state.availableIngredients = updatedAvailableIngredients;
 }
 
-function refreshAddIngredientDropdown(ingredientsContainer, activeIngredients, remainingIngredientOptions, dropdownContainer) {
+function renderIngredientDropdown(state) {
+  const availableIngredients = state.availableIngredients;
+  const dropdownContainer = state.dropdownContainer;
+
   // Clear the current dropdown and create a new one. This is done so that a clean event can be added to the dropdown's selection
   dropdownContainer.innerHTML = '';
   const addIngredientDropdownRoot = elementFactory.createAndAppendDropdownShell('custom-select', 'font-jersey', dropdownContainer);
@@ -192,10 +204,10 @@ function refreshAddIngredientDropdown(ingredientsContainer, activeIngredients, r
   // If the user selects an ingredient from the dropdown, add that ingredient to the list
   dropdownSelection.addEventListener('change', () => {
     const ingredientIdToAdd = Number(dropdownSelection.options[dropdownSelection.selectedIndex].value);
-    const updated = addToActiveIngredients(ingredientIdToAdd, activeIngredients, remainingIngredientOptions);
+    addIngredient(ingredientIdToAdd, state);
     
-    // Recreate the ingredients list with the now-removed ingredient
-    updateIngredientList(ingredientsContainer, updated.active, updated.options, dropdownContainer);
+    // Recreate the ingredients list with updated state that no longer has the removed ingredient in the active list
+    renderIngredientList(state);
   });
 
   // Create the starter option that is displayed when an ingredient is not selected
@@ -204,15 +216,15 @@ function refreshAddIngredientDropdown(ingredientsContainer, activeIngredients, r
   starterOption.textContent = 'Add another ingredient';
 
   // Sort the remaining ingredients array, then add them as options to the dropdwon
-  remainingIngredientOptions.sort((a, b) => a.name.localeCompare(b.name));
-  remainingIngredientOptions.forEach(ingredient => {
+  const sorted = [...availableIngredients].sort((a, b) => a.name.localeCompare(b.name));
+  sorted.forEach(ingredient => {
     const option = elementFactory.createAndAppendElement('option', null, dropdownSelection);
     option.value = ingredient.id;
     option.textContent = ingredient.name;
   });
 }
 
-function createIngredient(ingredientObject) {
+function renderIngredientRow(ingredientObject) {
   const ingredientDOMElement = ingredientRenderer.renderIngredient(ingredientObject);
 
   // Create the quantity input field with a 'x' symbol
