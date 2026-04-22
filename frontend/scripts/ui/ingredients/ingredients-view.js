ebc9076de7f37beeadf3ca61ee59f4c3cdeacd60
ccgstudiogit <trempe.connor@gmail.com>
2026-04-21 20:06:06 -0500

    Deleting items and showing the confirm delete modal working
    The confirm delete modal popup is now working again after starting the frontend refactor/redesign.

--- frontend/scripts/ui/ingredients/ingredients-view.js
+++ frontend/scripts/ui/ingredients/ingredients-view.js
@@ -1,7 +1,9 @@
 import * as baseView from '../shared/base-view.js';
 import * as elementFactory from '../../utils/element-factory.js';
+import * as buttonFactory from '../../utils/button-factory.js';
 import * as ingredientRenderer from '../../ui/ingredients/ingredient-render.js';
 import * as ingredientActions from '../../actions/ingredient-actions.js';
+import * as modalRenderer from '../components/modal.js';
 
 export function renderIngredientsView() {
   baseView.refresh();
@@ -20,15 +22,15 @@ export function renderAddForm() {
  * Displays all ingredients in the results section.
  * 
  * @async
- * @param {HTMLElement} parent The parent HTML element for the rendered ingredients.
+ * @param {HTMLElement} contentSection The parent HTML element for the rendered ingredients.
  * @returns {void}
  */
-async function displayIngredients(parent) {
+async function displayIngredients(contentSection) {
   // Fetch the ingredients from the backend via the actions layer, which calls the API layer
   try {
     const ingredients = await ingredientActions.getAllIngredients();
     const sorted = [...ingredients].sort((a, b) => a.name.localeCompare(b.name));
-    renderIngredients(sorted, parent);
+    renderIngredients(sorted, contentSection);
   } catch (message) {
     console.error(message);
   }
@@ -38,15 +40,49 @@ async function displayIngredients(parent) {
  * Fetch the ingredients from the backend, sort them by name, and render them. The ingredients' remove buttons are also added.
  * 
  * @async
- * @param {HTMLElement} parent The parent HTML element for the rendered ingredients.
+ * @param {HTMLElement} contentSection The parent HTML element for the rendered ingredients.
  * @returns {void}
  */
-async function renderIngredients(ingredients, parent) {
+async function renderIngredients(ingredients, contentSection) {
   ingredients.forEach(async ingredient => {
     const ingredientElement = ingredientRenderer.renderIngredient(ingredient);
-    parent.appendChild(ingredientElement.root);
+    contentSection.appendChild(ingredientElement.root);
     ingredientElement.removeButton.onclick = async function () {
-      //generateConfirmDeletePotionModal(ingredient, parent);
+      showConfirmDeleteModal(ingredient, contentSection);
     };
   });
 }
+
+/**
+ * Render the confirm delete modal window. If the user confirms the action, a DELETE request with the ingredient's id is sent to the backend.
+ * Once the confirm takes place, the ingredient list is re-rendered by calling displayAllIngredients again.
+ * 
+ * @param {Object} ingredient The ingredient object.
+ * @param {HTMLElement} contentSection The parent HTML element for the results section.
+ * @returns {void}
+ */
+function showConfirmDeleteModal(ingredient, contentSection) {
+  const confirmModal = modalRenderer.renderGlobalModal();
+
+  confirmModal.windowTitle.textContent = 'Confirm Delete';
+  confirmModal.windowText.textContent = `Delete ${ingredient.name} from the shop?`;
+
+  confirmModal.mainButton.textContent = 'Delete';
+  confirmModal.mainButton.onclick = async function () {
+    try {
+      await ingredientActions.deleteIngredient(ingredient.id);
+    } catch (message) {
+      console.error(message);
+    }
+
+    // Refresh the updated list after deletion
+    contentSection.innerHTML = '';
+    displayIngredients(contentSection);
+  }
+
+  // Add the cancel button
+  const cancelButton = buttonFactory.createAndAppendButton('Cancel', 'modal-button', confirmModal.buttonContainer, () => {
+    confirmModal.root.remove();
+  });
+}
+
--- frontend/scripts/ui/potions/potions-view.js
+++ frontend/scripts/ui/potions/potions-view.js
@@ -1,9 +1,11 @@
 import * as baseView from '../shared/base-view.js';
 import * as elementFactory from '../../utils/element-factory.js';
+import * as buttonFactory from '../../utils/button-factory.js';
 import * as potionRenderer from './potion-render.js';
 import * as potionAddForm from './potion-add-form.js';
 import * as potionEditForm from './potion-edit-form.js';
 import * as potionActions from '../../actions/potion-actions.js';
+import * as modalRenderer from '../components/modal.js';
 
 export function renderPotionsView() {
   baseView.refresh();
@@ -25,15 +27,15 @@ export function renderAddForm() {
 /**
  * Fetches all potions from the backend via the API layer and renders the potions under the input parent element.
  * 
- * @param {HTMLElement} parent The parent HTML element for the rendered potions.
+ * @param {HTMLElement} contentSection The parent HTML element for the rendered potions.
  * @returns {void}
  */
-async function displayPotions(parent) {
+async function displayPotions(contentSection) {
   // Fetch the potions from the backend via the actions layer, which calls the API layer
   try {
     const potions = await potionActions.getAllPotionsWithIngredients();
     const sorted = [...potions].sort((a, b) => a.name.localeCompare(b.name));
-    renderPotions(sorted, parent);
+    renderPotions(sorted, contentSection);
   } catch (message) {
     console.error(message);
   }
@@ -43,13 +45,13 @@ async function displayPotions(parent) {
  * Fetch the potions from the backend, sort them by name, and render them. The potions' edit and remove buttons are also configured.
  * 
  * @async
- * @param {HTMLElement} parent The parent HTML element for the rendered potions.
+ * @param {HTMLElement} contentSection The parent HTML element for the rendered potions.
  * @returns {void}
  */
-async function renderPotions(potions, parent) {
+async function renderPotions(potions, contentSection) {
   potions.forEach(async potion => {
     const potionElement = potionRenderer.renderPotion(potion);
-    parent.appendChild(potionElement.root);
+    contentSection.appendChild(potionElement.root);
 
     // If the edit button is pressed, open the edit potion form with that potion's information
     potionElement.editButton.onclick = function () {
@@ -59,7 +61,7 @@ async function renderPotions(potions, parent) {
     // If the remove button is pressed, prompt the user with a confirm delete request. If the user confirms,
     // delete the potion from the database (destructive)
     potionElement.removeButton.onclick = async function () {
-      //generateConfirmDeletePotionModal(potion, parent);
+      showConfirmDeleteModal(potion, contentSection);
     };
   });
 }
@@ -69,4 +71,37 @@ async function renderEditForm(potion) {
   const panel = baseView.renderDynamicPanel(baseView.getMainContent());
   
   potionEditForm.createEditPotionForm(potion, panel.content);
+}
+
+/**
+ * Render the confirm delete modal window. If the user confirms the action, a DELETE request with the potion's id is sent to the backend.
+ * Once the confirm takes place, the potion list is re-rendered by calling displayAllPotions again.
+ * 
+ * @param {Object} potion The potion object.
+ * @param {HTMLElement} contentSection The parent HTML element for the results section.
+ * @returns {void}
+ */
+function showConfirmDeleteModal(potion, contentSection) {
+  const confirmModal = modalRenderer.renderGlobalModal();
+
+  confirmModal.windowTitle.textContent = 'Confirm Delete';
+  confirmModal.windowText.textContent = `Delete ${potion.name} from the shop?`;
+
+  confirmModal.mainButton.textContent = 'Delete';
+  confirmModal.mainButton.onclick = async function () {
+    try {
+      await potionActions.deletePotion(potion.id);
+    } catch (message) {
+      console.error(message);
+    }
+
+    // Refresh the updated list after deletion
+    contentSection.innerHTML = '';
+    displayPotions(contentSection);
+  };
+
+  // Add the cancel button
+  const cancelButton = buttonFactory.createAndAppendButton('Cancel', 'modal-button', confirmModal.buttonContainer, () => {
+    confirmModal.root.remove();
+  });
 }
